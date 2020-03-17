4ee04465069f22f34e3084c57b7e33e31cd691ef
Mzito <you@example.com>
2020-03-17 07:20:17 +0200

    initial commit

---
+++ public/app.js
@@ -0,0 +1,58 @@
+"use strict"
+
+const dotenv = require('dotenv').config();
+const Pool = require("pg").Pool;
+const pool = new Pool();
+
+pool.connect((err,res)=>{
+	if(err){
+		console.log(err)
+	}console.log(res);
+});
+
+const addNewVisitor  = async(vName,vAge,dateOfVisit,timeOfVisit,assistantName,comments) =>{
+	return new Promise(async(resolve, reject)=>{
+	  await pool.query(`INSERT INTO VISITORS( vName, vAge, dateOfVisit, timeOfVisit, assistantName, comments) VALUES($1,$2,$3,$4,$5,$6) RETURNING *` , 
+	  [vName,vAge,dateOfVisit,timeOfVisit,assistantName,comments],
+	  (err,results)=> {
+		if (err) {
+			reject(err);
+		} 
+		console.log(results.rows[0]);
+		resolve(results[0]);
+		});
+	})	  
+}
+const listAllVisitors = async() => {
+	return new Promise(async(resolve,reject)=>{
+	  await pool.query(
+		`SELECT * FROM VISITORS`,
+		(err, results) => {
+		if (err) {
+			reject(err);
+		}
+		resolve(results.rows);	
+	   	}
+	);
+	})
+};
+
+
+
+const deleteVisitor = async (id) => {
+	return new Promise(async(resolve,reject)=>{
+		await pool.query(
+			`DELETE FROM visitors WHERE id = $1`,
+			[id],
+			(err, results) => {
+			  if (err) {
+				reject(err);
+			  }
+			  console.log('deleted!!!')
+			  resolve(results);
+			}
+		  );
+	}
+)};
+
+module.exports = {addNewVisitor,listAllVisitors,deleteVisitor}
